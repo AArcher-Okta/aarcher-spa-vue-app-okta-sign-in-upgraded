de6950876ae2e0f226cd29bf07570989941ccb9b
Alex Archer <alex.archer@okta.com>
2022-03-17 17:11:31 -0400

    style:fix disabled button styling

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    <h2>About Section</h2>
+    <p>The Okta Sign-In Widget is a JavaScript library that gives you a fully-featured and customizable sign-in experience, which you can use to authenticate users on any website.</p>
   </div>
 </template>
+
+<style scoped>
+  p {
+    max-width: 700px;
+    display: inline-flex;
+    justify-content: center;
+    text-align: center;
+  }
+</style>
